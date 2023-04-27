9e0f9ac723f58e950715dbbb645aa74b20a4bd21
Murad CDC <atamurad.babakulyyev@gmail.com>
2023-04-27 14:55:58 -0400

    add text

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -5,8 +5,8 @@ import { Helmet } from 'react-helmet'
 function WelcomePage() {
   const onButtonClick = () => {}
   return (
-    <section className='bg-lime-50 h-screen flex flex-col justify-between '>
-      <header className='bg-image text-gray-50  '>
+    <section className='bg-lime-50 h-screen  flex flex-col justify-between '>
+      <header className='bg-image text-gray-50 pb-12  '>
         <div className='inner-header fade-up w-4/5 md:w-2/3  m-auto '>
           <div className='logo mt-6'>
             <h1 className=' text-gray-50  text-2xl md:text-4xl text-center font-semibold'>
@@ -27,19 +27,20 @@ function WelcomePage() {
           </div>
         </div>
       </header>
+      <div className='content-1 px-6 py-10'>
+        <h3 className='text-left text-xl mb-4'>Start with a free CDL permit practice test!</h3>
+        <p className=' text-sm'>We offer a variety of sample permit test questions and online cheat sheets to help you prepare for the real exam.</p>
+      </div>
+      <div className='content-2 bg-gray-900 text-gray-100 px-4 py-6 '>
+        <h4 className='text-2xl text-center font-medium mb-6'>Studying for the permit test?</h4>
+        <p>
+          At CDL Permit Pro, we understand how challenging it can be to pass the CDL permit test on your first try. That's why we've created a comprehensive online practice test platform to help you prepare and gain the confidence you need to ace the exam. Our app offers a variety of CDL permit
+          practice tests covering all topics, including general knowledge, air brakes, and hazardous materials. We also provide detailed explanations and instant feedback to help you identify your weaknesses and improve your knowledge. With CDL Permit Pro, you'll have everything you need to pass
+          your CDL permit test with confidence. Try it today and get ready to start your journey on the open road!
+        </p>
+      </div>
     </section>
   )
 }
 
 export default WelcomePage
-{
-  /* <div className='flex flex-col justify-center items-left h-1/2  px-8 '>
-<h1 className='fade-up font-bold text-3xl text-lime-950 '>CDL pro</h1>
-<h2 className='fade-up font-bold text-xl italic text-lime-100 ml-5 mt-1'>Ace your permit test with our straightforward solution</h2>
-</div>
-<div className='h-1/3  flex items-center justify-center '>
-<Link to='/cdl' className='fade-up  text-center bg-lime-100 px-12 py-3 rounded-md w-4/5 mb-2 text-lime-950 font-semibold'>
-  Start Now
-</Link>
-</div> */
-}
