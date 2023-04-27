60cf4407723f0b4a2f65b460726f1e360677bc56
Murad CDC <atamurad.babakulyyev@gmail.com>
2023-04-27 14:14:24 -0400

    Add helmet

--- src/App.js
+++ src/App.js
@@ -5,17 +5,33 @@ import ExamParts from './components/ExamParts'
 import StatesPage from './components/StatesPage'
 import Exam from './components/Exam'
 import NotFoundPage from './components/NotFoundPage'
+import { Helmet } from 'react-helmet'
+import { Fragment } from 'react'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<WelcomePage />} />
-      <Route path='/cdl' element={<StatesPage />} />
-      <Route path='/cdl/:usersState' element={<ExamTypePage />} />
-      <Route path='/cdl/:usersState/:examType' element={<ExamParts />} />
-      <Route path='/cdl/:usersState/:examType/:examPart' element={<Exam />} />
-      <Route element={<NotFoundPage />} />
-    </Routes>
+    <Fragment>
+      <Helmet>
+        <title>CDL Permit Pro - Pass Your CDL Permit Test with Confidence</title>
+        <meta
+          name='description'
+          content="CDL Permit Pro is a comprehensive online practice test platform designed to help you pass your CDL permit test with confidence. Our app offers a variety of CDL permit practice tests covering all topics, including general knowledge, air brakes, and hazardous materials. With detailed explanations and instant feedback, you'll be able to identify your weaknesses and improve your knowledge. Try CDL Permit Pro today and get ready to ace your CDL permit test\!"
+        />
+        <meta name='keywords' content='CDL, Permit, Test, Practice, Platform , Truck permit ,road test, road permit test' />
+        <meta name='robots' content='index, follow' />
+        <meta name='author' content='Atamurad B' />
+        <meta name='language' content='en' />
+        <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+      </Helmet>
+      <Routes>
+        <Route path='/' element={<WelcomePage />} />
+        <Route path='/cdl' element={<StatesPage />} />
+        <Route path='/cdl/:usersState' element={<ExamTypePage />} />
+        <Route path='/cdl/:usersState/:examType' element={<ExamParts />} />
+        <Route path='/cdl/:usersState/:examType/:examPart' element={<Exam />} />
+        <Route element={<NotFoundPage />} />
+      </Routes>
+    </Fragment>
   )
 }
 
